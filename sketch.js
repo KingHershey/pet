37badcbd217db45553fe45bedea1fc711f3661bf
KingHershey <65794616+KingHershey@users.noreply.github.com>
2020-11-23 22:40:35 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//Create variables here
+var dog
+var happyDog
+var database
+var foodS
+var foodStock
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
